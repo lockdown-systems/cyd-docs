06beb9cb53992f2c9178216d0556a3e6ae051205
Micah Lee <micah@micahflee.com>
2026-04-24 08:24:50 -0700

    Fix deprecation warning

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,11 @@ const config: Config = {
   url: "https://docs.cyd.social",
   baseUrl: "/",
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
